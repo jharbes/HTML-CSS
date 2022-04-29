4583cf933f154e6d9312bd90ec4ec2a13e435182
Jorge Harbes <jharbes@hotmail.com>
2022-04-29 16:00:03 -0300

    Initial commit

---
+++ EXERCICIOS/EX018/teste.js
@@ -0,0 +1,17 @@
+a = 10
+
+i = 0
+
+while (i < 10){
+
+           while (i < 10){
+
+                       a = a + 1
+
+                       i = i + 1
+
+           }
+
+        }
+
+console.log(a)
